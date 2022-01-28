f2d1b419a7dee5d048bd4d9d23d94d22d97743ca
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-01-28 12:59:05 +0000

    added else if grades

---
+++ grades.js
@@ -0,0 +1,13 @@
+const grade = (num) => {
+    if (num) > 90 && num <= 100){
+        return "A";
+    else if (num) > 80 && num <= 90) {
+        return "B";
+    else if (num) > 70 && num <= 80 ) {
+        return "C";
+    else if (num) > 60 && num <= 70) {
+        return "D";
+    else 
+        return "F";
+        )
+    };
