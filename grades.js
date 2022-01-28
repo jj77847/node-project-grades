82bd69b3bd2a2f691da01612bf9d04dc3e53bed7
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-01-28 13:01:14 +0000

    syntax errors grades

--- grades.js
+++ grades.js
@@ -1,11 +1,11 @@
 const grade = (num) => {
-    if (num) > 90 && num <= 100){
+    if (num) > 90 && num <= 100) {
         return "A";
-    else if (num) > 80 && num <= 90) {
+    else if (num) > 80 && num <= 90){
         return "B";
-    else if (num) > 70 && num <= 80 ) {
+    else if (num) > 70 && num <= 80 ){
         return "C";
-    else if (num) > 60 && num <= 70) {
+    else if (num) > 60 && num <= 70){
         return "D";
     else 
         return "F";
